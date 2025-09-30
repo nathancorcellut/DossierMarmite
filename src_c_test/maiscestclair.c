f202109377c52ad24e17e33571c940524e17ffed
nathancorcellut <nathan.corcellut@gmail.com>
2025-09-30 17:03:44 +0200

    phrase conclusion

--- src_c_test/maiscestclair.c
+++ src_c_test/maiscestclair.c
@@ -5,5 +5,6 @@ int main(){
     printf("Attention ce soir Boris danse !");
     printf("Mais c'est impossible Uncle Tom là!");
     /*printf("La GRANDE classe le commit a la mano !");*/
+    printf("Et voila les enfants, c'était la Aventure!");
     return 1;
 }
