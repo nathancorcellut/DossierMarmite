14f8c1ad5a0ed559ae61be00e7271a29be16ddf2
nathancorcellut <43115468+nathancorcellut@users.noreply.github.com>
2025-09-30 17:18:26 +0200

    joliment embetement

--- src_c_test/maiscestclair.c
+++ src_c_test/maiscestclair.c
@@ -8,5 +8,7 @@ int main(){
     printf("Et voila les enfants, c'était la Aventure!");
     printf("La ligne 9 écrite depuis GitHub dans l'editeur");
     printf("Le manual correcteur a decidé que ca suffit ainsi");
+
+    print("La on va s'embeter joliment");
     return 1;
 }
