dd762b92bdb60d73f41d4a576507c3e5cb773968
nathancorcellut <nathan.corcellut@gmail.com>
2025-09-30 16:56:53 +0200

    first manual commit

--- src_c_test/maiscestclair.c
+++ src_c_test/maiscestclair.c
@@ -4,5 +4,6 @@ int main(){
     printf("Top délire méga groove !");
     printf("Attention ce soir Boris danse !");
     printf("Mais c'est impossible Uncle Tom là!");
+    printf("La GRANDE classe le commit a la mano !");
     return 1;
 }
