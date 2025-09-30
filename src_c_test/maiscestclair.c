2122c8850e4923437dbf16c38de57197f5f0b200
nathancorcellut <nathan.corcellut@gmail.com>
2025-09-30 17:20:15 +0200

    essai avant merge joli

--- src_c_test/maiscestclair.c
+++ src_c_test/maiscestclair.c
@@ -8,5 +8,7 @@ int main(){
     printf("Et voila les enfants, c'était la Aventure!");
     printf("La ligne 9 écrite depuis GitHub dans l'editeur");
     printf("Le manual correcteur a decidé que ca suffit ainsi");
+
+    printf("De mon cote je ne sais pas que je vais etre embeté!");
     return 1;
 }
